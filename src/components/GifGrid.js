b0767d9a0ce62edab73c995d26a7cfe83c3c284c
JohnGRP <john-anthonygarcia@hotmail.com>
2022-02-20 19:03:54 -0400

    animaciones y tiempo de carga

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,32 +1,35 @@
 import React, { useEffect, useState } from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
-import { getGifs } from './helpers/getGifd';
+// import { getGifs } from './helpers/getGifd';
 
 export const GifGrid = ( {category} ) => {
 
-    const [images, setImages] = useState([]);
+    // const [images, setImages] = useState([]);
+    const {loading, data} = useFetchGifs( category );
+    // console.log(loading);
+    // console.log(data);
 
 
-    useEffect ( ()=> {
-        getGifs ( category )
-            .then( setImages )
-    },[category] )
 
 
 
     return (
         <>
             <h3> { category } </h3>
+
+            { loading && <p> Cargado...</p> }
+
             <div className='card-grid'>
-            {
-                images.map( img => (
-                    <GifGridItem 
-                        key={ img.id }
-                        { ...img }
-                    />
-                ))
-            }
-        </div>
+                {
+                    data.map( img => (
+                        <GifGridItem 
+                            key={ img.id }
+                            { ...img }
+                        />
+                    ))
+                }
+            </div>
         </>
     );
 };
---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { getGifs } from '../components/helpers/getGifs';
+
+export const useFetchGifs = ( category) => {
+    
+    const [state, setState] = useState({
+        data: [],
+        loading: true
+    });
+
+
+    useEffect ( ()=> {
+
+        getGifs ( category )
+            .then( imgs => {
+
+                setTimeout( ()=> {
+
+                    setState( {
+                        data: imgs,
+                        loading: false
+                    });
+
+                },3000 );
+
+            })
+    },[category] )
+
+    // setTimeout(() => {
+    //     setState({
+    //         data: [1,2,3,4,5,6,7,8,9],
+    //         loading: false
+    //     })
+    // }, 3000);
+
+    return state;
+
+}
